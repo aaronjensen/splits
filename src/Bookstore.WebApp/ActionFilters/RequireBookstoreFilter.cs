df691d3d55da52e90966799fe5df9b4d57a44f6d
Aaron Jensen <aaronjensen@gmail.com>
2009-12-10 20:50:15 -0800

    UrlStrong: Upgrading

--- src/Bookstore.WebApp/ActionFilters/RequireBookstoreFilter.cs
+++ src/Bookstore.WebApp/ActionFilters/RequireBookstoreFilter.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web.Mvc;
+using Bookstore.Application;
+using Bookstore.Application.Queries;
 using Microsoft.Practices.ServiceLocation;
 
 namespace Bookstore.WebApp.ActionFilters
@@ -10,6 +12,7 @@ namespace Bookstore.WebApp.ActionFilters
   public class RequireBookstoreFilter : ActionFilterAttribute
   {
     readonly ICache _cache;
+    readonly IQuerier _querier;
 
     public RequireBookstoreFilter()
     {
@@ -19,10 +22,19 @@ namespace Bookstore.WebApp.ActionFilters
     public override void OnActionExecuting(ActionExecutingContext filterContext)
     {
       var bookStore = _cache.BookStore;
-      if (bookStore == null)
+      if (bookStore != null) return;
+
+      bookStore = _querier.Get(new ThisBookStoreQuery());
+
+      if (bookStore != null)
       {
-        
+        _cache.BookStore = bookStore;
+        return;
       }
+
+      filterContext.Result = new RedirectResult(Urls.root)
+
+        
     }
   }
 }
