34544a4a25066538f99a5cfc8bb11a40f2d32512
Jacob Lewallen <jlewalle@gmail.com>
2009-12-28 19:05:46 -0800

    Moving more stuff to internal.

--- src/Splits/Web/StepHandlers/RenderViewStepHandler.cs
+++ src/Splits/Web/StepHandlers/RenderViewStepHandler.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Web.Mvc;
+using Splits.Internal;
 using Splits.Web.Steps;
-using SparkViewFactory = Splits.Web.Spark.SparkViewFactory;
 
 namespace Splits.Web.StepHandlers
 {
@@ -26,46 +25,5 @@ namespace Splits.Web.StepHandlers
       _viewRenderer.RenderViewData(stepContext, viewData, step.ViewName);
       return Continuation.Continue;
     }
-  }
-
-  public interface IViewRenderer
-  {
-    void RenderModel(StepContext stepContext, object model, string viewName);
-    void RenderViewData(StepContext stepContext, ViewDataDictionary view, string viewName);
-  }
-
-  public class ViewRenderer : IViewRenderer
-  {
-    readonly static SparkViewFactory _factory = new SparkViewFactory();
-
-    public void RenderViewData(StepContext stepContext, ViewDataDictionary viewData, string viewName)
-    {
-      var view = _factory.FindView(stepContext.RequestContext, stepContext.UrlStrongPath, viewName, String.Empty, true, true);
-      if (view.View == null)
-      {
-        var locations = new StringBuilder();
-        foreach (var location in view.SearchedLocations)
-        {
-          locations.AppendLine();
-          locations.Append(location);
-        }
-        throw new InvalidOperationException("No view could be found in: " + locations);
-      }
-
-      var controllerContext = new ControllerContext(stepContext.RequestContext, FakeController);
-      var viewContext = new ViewContext(controllerContext, view.View, viewData, new TempDataDictionary());
-      view.View.Render(viewContext, stepContext.Response.Output);
-    }
-
-    public void RenderModel(StepContext stepContext, object model, string viewName)
-    {
-      var viewData = new ViewDataDictionary();
-      viewData.Model = model;
-      stepContext.Fill(viewData);
-      RenderViewData(stepContext, viewData, viewName);
-    }
-
-    static readonly FakeControllerForControllerContext FakeController = new FakeControllerForControllerContext();
-    class FakeControllerForControllerContext : Controller { }
   }
 }
