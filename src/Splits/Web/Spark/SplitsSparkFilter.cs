06c8c14febfb02507a8a0b043618786f62aba3c9
Jacob Lewallen <jlewalle@gmail.com>
2010-01-11 18:29:04 -0800

    A flatter way of doing Display/EditorTemplates? Not sure about this.

--- src/Splits/Web/Spark/SplitsSparkFilter.cs
+++ src/Splits/Web/Spark/SplitsSparkFilter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
 using Spark.Web.Mvc.Descriptors;
+using System.Linq;
 
 namespace Splits.Web.Spark
 {
@@ -16,10 +17,22 @@ namespace Splits.Web.Spark
 
     public override IEnumerable<string> PotentialLocations(IEnumerable<string> locations, IDictionary<string, object> extra)
     {
-      foreach (var location in locations)
+      foreach (var location in PotentialLocationsInternal(locations, extra).Distinct())
+      {
+        yield return location;
+      }
+    }
+
+    public IEnumerable<string> PotentialLocationsInternal(IEnumerable<string> locations, IDictionary<string, object> extra)
+    {
+      foreach (var location in locations.Select(l => l.Replace(@"\", "/")))
       {
         if (extra.ContainsKey("urlStrongPath"))
         {
+          if (location.Contains("/DisplayTemplates/"))
+            yield return location.Replace("/DisplayTemplates", "").Replace(".spark", ".display.spark");
+          if (location.Contains("/EditorTemplates/"))
+            yield return location.Replace("/EditorTemplates", "").Replace(".spark", ".editor.spark");
           yield return location.Replace(@"Splits\", extra["urlStrongPath"] + @"\");
         }
         else
