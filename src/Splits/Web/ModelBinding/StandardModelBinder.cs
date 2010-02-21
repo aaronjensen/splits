18a231fbeb0847e2374f1d6e71d051e41a53cec3
Jacob Lewallen <jlewalle@gmail.com>
2010-02-20 23:07:23 -0800

    Collection binding... only binding arrays right now.

--- src/Splits/Web/ModelBinding/StandardModelBinder.cs
+++ src/Splits/Web/ModelBinding/StandardModelBinder.cs
@@ -117,8 +117,23 @@ namespace Splits.Web.ModelBinding
 
     public object ConvertValue(Type type, string name, object rawValue, IDictionary<string, object> data, string prefix)
     {
-      var converter = _converters[type];
+      System.Diagnostics.Trace.WriteLine("ConvertValue " + prefix + " " + name);
+      if (IsCollectionType(type))
+      {
+        var elementType = GetElementType(type);
+        var values = new System.Collections.ArrayList();
+        for (var i = 0; ; ++i)
+        {
+          var temp = new ReadCountingDictionary(data);
+          var found = ConvertValue(elementType, name + "[" + i + "]", rawValue, temp, prefix);
+          if (found == null || !temp.AnyReads)
+            break;
+          values.Add(found);
+        }
+        return values.ToArray(elementType);
+      }
 
+      var converter = _converters[type];
       if (converter == null)
       {
         return Bind(type, data, AddPrefix(prefix, name)).Value;
@@ -128,6 +143,109 @@ namespace Splits.Web.ModelBinding
         TargetName = name,
         Value = rawValue
       });
+    }
+
+    static bool IsCollectionType(Type type)
+    {
+      return type.IsArray;
+    }
+
+    static Type GetElementType(Type type)
+    {
+      return type.GetElementType();
+    }
+  }
+
+  public class ReadCountingDictionary : IDictionary<string, object>
+  {
+    readonly IDictionary<string, object> _target;
+    Int32 _reads;
+
+    public bool AnyReads
+    {
+      get { return _reads > 0; }
+    }
+
+    public ReadCountingDictionary(IDictionary<string, object> target)
+    {
+      _target = target;
+    }
+
+    public bool TryGetValue(string key, out object value)
+    {
+      return _target.TryGetValue(key, out value);
+    }
+
+    public object this[string key]
+    {
+      get
+      {
+        if (_target.ContainsKey(key))
+          _reads++;
+        return _target[key];
+      }
+      set { throw new NotImplementedException(); }
+    }
+
+    bool IDictionary<string, object>.ContainsKey(string key)
+    {
+      return _target.ContainsKey(key);
+    }
+
+    void ICollection<KeyValuePair<string, object>>.Add(KeyValuePair<string, object> item)
+    {
+      throw new NotImplementedException();
+    }
+
+    void ICollection<KeyValuePair<string, object>>.Clear()
+    {
+      throw new NotImplementedException();
+    }
+
+    bool ICollection<KeyValuePair<string, object>>.Contains(KeyValuePair<string, object> item)
+    {
+      throw new NotImplementedException();
+    }
+
+    void ICollection<KeyValuePair<string, object>>.CopyTo(KeyValuePair<string, object>[] array, int arrayIndex)
+    {
+      throw new NotImplementedException();
+    }
+
+    bool ICollection<KeyValuePair<string, object>>.Remove(KeyValuePair<string, object> item)
+    {
+      throw new NotImplementedException();
+    }
+
+    int ICollection<KeyValuePair<string, object>>.Count { get { throw new NotImplementedException(); } }
+
+    bool ICollection<KeyValuePair<string, object>>.IsReadOnly { get { throw new NotImplementedException(); } }
+
+    void IDictionary<string, object>.Add(string key, object value)
+    {
+      throw new NotImplementedException();
+    }
+
+    bool IDictionary<string, object>.Remove(string key)
+    {
+      throw new NotImplementedException();
+    }
+
+    ICollection<string> IDictionary<string, object>.Keys
+    {
+      get { throw new NotImplementedException(); }
+    }
+
+    ICollection<object> IDictionary<string, object>.Values { get { throw new NotImplementedException(); } }
+
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    {
+      return ((IEnumerable<KeyValuePair<string, object>>)this).GetEnumerator();
+    }
+
+    IEnumerator<KeyValuePair<string, object>> IEnumerable<KeyValuePair<string, object>>.GetEnumerator()
+    {
+      throw new NotImplementedException();
     }
   }
 }
